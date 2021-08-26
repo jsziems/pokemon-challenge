41a36074415e86547a6912246bd1b1449c882ff1
Janet Ziems <jsziems@gmail.com>
2021-08-26 14:56:39 -0400

    Initial push

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,38 +9,75 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      count: 0
     }
   }
 
-  fetchPokemon() {
-    let min = Math.ceil(1);
-    let max = Math.floor(152);
-    let pokeNum = Math.floor(Math.random() * (max - min) + min);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
-      method: 'GET'
-    }).then(res => res.json())
-      .then(res => {
-        this.setState({
-          pokeInfo: res,
-          pokeSprite: res.sprites.front_default,
-          pokeName: res.species.name,
-        })
-      })
-      .catch((err) => console.log(err))
+  componentDidUpdate() {
+
+    console.log('Updating')
   }
 
-  render() {
+  countDown = () => {
+    const interval = setInterval(() => {
+      if (this.state.count > 0) {
+        this.setState(prevState => ({
+          count: prevState.count - 1
+        }))
+      } else {
+        clearInterval(interval)
+      }
+
+    }, 1000)
+  }
+
+
+async fetchPokemon() {
+  let min = Math.ceil(1);
+  let max = Math.floor(152);
+  let pokeNum = Math.floor(Math.random() * (max - min) + min);
+  let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
+    method: 'GET'
+  })
+  let json = await res.json()
+  console.log(json)
+      this.setState({
+        pokeInfo: json,
+        pokeSprite: json.sprites.front_default,
+        pokeName: json.species.name,
+        count: 3,
+      })
+      this.countDown()
+    
+}
+
+render() {
+  if (this.state.count != 0) {
     return (
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <button className={'start'}
+          onClick={() => { this.fetchPokemon() }}>Start!</button>
+        <h1 className={'timer'} >{this.state.count}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
+          <img className={'pokeShadow'} src={this.state.pokeSprite} />
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className={'wrapper'}>
+        <button className={'start'}
+          onClick={() => { this.fetchPokemon() }}>Start!</button>
+        <h1 className={'timer'} >{this.state.count}</h1>
+        <div className={'pokeWrap'}>
+          <img className={'pokeImage'} src={this.state.pokeSprite} />
           <h1 className={'pokeName'}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
   }
+}
 }
 
 export default PokeFetch;
